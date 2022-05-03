cf588b6c67473e0ace2f710da77c09b5ebfcc241
Robbie Prokop <robbieprokop@gmail.com>
2022-05-03 22:09:04 +0000

    #5 solved

--- 4_queries/5_AVG_duration_per_cohort.sql
+++ 4_queries/5_AVG_duration_per_cohort.sql
@@ -1,6 +1,7 @@
 SELECT cohorts.name, AVG(completed_at - started_at) as average_assistance_request_duration
-  FROM students;
-  JOIN cohorts ON cohorts.id = cohort_id;
-  JOIN assistance_requests ON students.id = student_id;
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  JOIN assistance_requests ON students.id = assistance_requests.student_id
+  GROUP BY cohorts.name;
 
   
