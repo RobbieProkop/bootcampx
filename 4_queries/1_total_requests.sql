29b634bb361d1d5cca4d7fa5ff7b21330fcb304f
Robbie Prokop <robbieprokop@gmail.com>
2022-05-03 17:06:12 +0000

    group by total assistance_requests

--- 4_queries/1_total_requests.sql
+++ 4_queries/1_total_requests.sql
@@ -1,2 +1,5 @@
-SELECT count(*) 
-FROM assistance_requests;
+SELECT count(assistance_requests.*) as total_assistances, teachers.name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name;
